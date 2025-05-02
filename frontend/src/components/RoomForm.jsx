2875ec046d94d900b88098d14399f639956aac89
UzairKhan <uzaircodes0110@gmail.com>
2025-05-02 16:25:00 +0530

    prod

--- frontend/src/components/RoomForm.jsx
+++ frontend/src/components/RoomForm.jsx
@@ -19,7 +19,7 @@ const RoomForm = ({ onJoinRoom }) => {
     setIsCreating(true);
     
     try {
-      const response = await fetch('http://localhost:3000/api/room', { //process.env.prod 
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/room`, { //process.env.prod 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -56,7 +56,7 @@ const RoomForm = ({ onJoinRoom }) => {
     }
     
     try {
-      const response = await fetch(`http://localhost:3000/api/room/${roomId.trim()}`, {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/room/${roomId.trim()}`, {
         method: 'GET'
       });
       
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  },
   define : {
     global: {},
   }
